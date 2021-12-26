ff6c748c31106a8f357faed17497389ef68d5ee0
bkc-tomi <bkc.tomi1979@gmail.com>
2021-12-26 21:59:07 +0900

    first commmit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import 'katex/dist/katex.min.css';
 import Latex from 'react-latex-next';
+import Img from "../img/GitHub-Mark-Light-32px.png";
 
 const Header = () => {
     const latex = `$\\LaTeX$ Image Generator`;
     return (
-        <div className="bg-green-500 w-screen h-16">
+        <div className="bg-green-500 w-screen h-16 flex justify-between">
             <h1 className="text-xl md:text-3xl font-serif font-bold text-white p-2">
                 <Latex>
                     { latex }
                 </Latex>
             </h1>
+            <div className="w-16 h-16 flex justify-center items-center">
+                <a
+                    href=""
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src={ Img } />
+                </a>
+            </div>
         </div>
     );
 }
